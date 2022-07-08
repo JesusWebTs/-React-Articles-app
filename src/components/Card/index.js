36e409ace2513dc5a443e12316fb9a7db836f888
xzotacx <jesus.152006@gmail.com>
2022-07-07 21:43:05 -0400

    agregando ultimos detalles de diseño

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,12 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import Loader from "../Loader";
 import "./styles.css";
 
 function Card({ cardInfo }) {
   const { category, title, image, content, url, createdAt, id } = cardInfo;
   let [_category, setCategory] = useState(category);
+  const [loaded, setLoaded] = useState(true);
+  const [_img, setImg] = useState();
+
   return (
     <article className="card-container" id={id}>
-      <img className="card-container__img" src={image} />
+      <div className="car-container__img-container">
+        <img
+          className="card-container__img"
+          src={image}
+          onLoad={() => setLoaded(false)}
+        />
+        {loaded ? (
+          <div className="card-container__loader">
+            <Loader />
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
       <div className="card-container__text">
         <header className="card-container__header">
           <h3>{title}</h3>
